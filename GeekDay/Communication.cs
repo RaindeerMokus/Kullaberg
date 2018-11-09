a8e35f337f4ee28d3e827978afaa66382194157e
DESKTOP-8N9Q0MG\Győri Ferenc <szlggyogyo12@gmail.com>
2018-11-09 20:37:05 +0100

    Json deserialization

--- GeekDay/Communication.cs
+++ GeekDay/Communication.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,65 +26,24 @@ namespace GeekDay
             {
                 var remoteEP = new IPEndPoint(IPAddress.Any, port);
                 var data = udpServer.Receive(ref remoteEP);
-                //  Console.WriteLine(data.ToString());
-             //   var str = MyClass.Desserialize(data).Id + "" + MyClass.Desserialize(data).Name + "" + MyClass.Desserialize(data).Data;
-                
                 Console.WriteLine();
                 Console.WriteLine("DataStart----------------------------------------------------");
-                string datastring = System.Text.Encoding.UTF8.GetString(data);
-                Console.WriteLine(datastring);
+                StreamReader reader = new StreamReader(new MemoryStream(data), Encoding.Default);
+                Dictionary<string, object> actual = new JsonSerializer().Deserialize<Dictionary<string, object>>(new JsonTextReader(reader));
+
+                foreach (var item in actual)
+                {
+                    Console.WriteLine(item.Key+":"+item.Value);
+                }
                 Console.WriteLine("DataEnd----------------------------------------------------");
                 Console.WriteLine();
-                //udpServer.Send(new byte[] { 1 }, 1, remoteEP); // if data is received reply letting the client know that we got his data          
                 Console.ReadLine();
             }
         }
 
     }
-    public class MyClass
+    public class Json
     {
-
-        /*public int Id { get; set; }
-        public string Name { get; set; }
-        public string Data { get; set; }*/
-      //  public List<string> Datas { get; set; }
-        /*  public byte[] Serialize()
-          {
-              using (MemoryStream m = new MemoryStream())
-              {
-                  using (BinaryWriter writer = new BinaryWriter(m))
-                  {
-                      writer.Write(Id);
-                      writer.Write(Name);
-                  }
-                  return m.ToArray();
-              }
-          }*/
-
-        public static List<string> Desserialize(byte[] data)
-        {
-            MyClass result = new MyClass();
-            List<string>Datas = new List<string>();
-            using (MemoryStream m = new MemoryStream(data))
-            {
-                using (BinaryReader reader = new BinaryReader(m))
-                {
-                    try
-                    {
-                        while (true)
-                        {
-                            Datas.Add(reader.ReadString());
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("\n"+e.StackTrace+"\n" + e.ToString());
-                        return Datas; 
-                    }
-                }
-            }
-            
-        }
-
+      
     }
 }
