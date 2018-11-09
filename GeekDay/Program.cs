dcb1bf1395dc2d809ce4511e571b82e99a29a1ff
DESKTOP-8N9Q0MG\Győri Ferenc <szlggyogyo12@gmail.com>
2018-11-09 18:32:53 +0100

    Start

---
+++ GeekDay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Communication communication = new Communication();
+            communication.Connect();
+        }
+    }
+}
